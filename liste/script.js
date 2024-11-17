0d93ef06f1adc691c603103a3f3fd2d733e76b7f
remy <remynderyt@gmail.com>
2024-11-17 15:00:08 +0100

    noel

--- liste/script.js
+++ liste/script.js
@@ -1,14 +1,22 @@
 function init() {
-    let list = document.querySelectorAll('li');
+    let list = document.querySelectorAll('img');
     console.log(list);
     list.forEach(element => {
+        console.log(element);
         element.addEventListener('click',clicker)
     });
     
 }
 
 function clicker(event) {
+    let list = document.querySelectorAll('img');
+    console.log(list);
+    list.forEach(element => {
+        element.setAttribute('style','box-shadow: 0;')
+    });
+
+
     let target = event.target;
     console.log(target);
-    target.setAttribute('style', 'box-shadow: 10px 5px 5px red;');
+    target.setAttribute('style', 'box-shadow: 15px 15px 8px grey;');
 }
